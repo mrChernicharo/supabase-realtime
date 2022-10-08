d5c57aa8117bac9059dbbef4a68e46206f21cd30
Felipe Chernicharo <string7dev@gmail.com>
2022-10-07 21:35:51 -0400

    showing presence

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,45 @@ import { createClient } from '@supabase/supabase-js';
 import { createSignal } from 'solid-js';
 
 import { supabase } from './supabaseClient';
-// The name of the channel can be set to any string.
-// Set the same name across both the broadcasting and receiving clients.
-const channel = supabase.channel('user1-location', {
-	configs: {
-		broadcast: { ack: true },
-	},
-});
-
-channel
-	// .on('broadcast', { event: 'location' }, (payload) => console.log(payload))
-	.subscribe(async status => {
-		console.log(status);
-		if (status === 'SUBSCRIBED') {
-			// sending a new message every second
-			setInterval(async () => {
-				const message = await channel.send({
-					type: 'broadcast',
-					event: 'location',
-					payload: { x: Math.random(), y: Math.random() },
-				});
-				console.log(message);
-			}, 1000);
-		}
-	});
 
 function App() {
 	const [professionalEmail, setProfessionalEmail] = createSignal('');
+	const [currentUsers, setCurrentUsers] = createSignal([]);
+
+	const channel = supabase.channel('online-users');
+
+	function getRandomUser() {
+		const users = ['Alice', 'Bob', 'Mallory', 'Inian'];
+		return users[Math.floor(Math.random() * users.length)];
+	}
+
+	channel
+		.on('presence', { event: 'sync' }, () => {
+			console.log('currently online users', channel.presenceState());
+			setCurrentUsers(prev => Object.values(channel.presenceState()));
+		})
+		.on('presence', { event: 'join' }, ({ newPresences }) => {
+			console.log(
+				'new users have joined',
+				{ newPresences },
+				channel.presenceState()
+			);
+		})
+		.on('presence', { event: 'leave' }, ({ leftPresences }) => {
+			console.log(
+				'users have left',
+				{ leftPresences },
+				channel.presenceState()
+			);
+		})
+		.subscribe(async status => {
+			if (status === 'SUBSCRIBED') {
+				const message = await channel.track({
+					user_name: getRandomUser(),
+				});
+				console.log(message);
+			}
+		});
 
 	return (
 		<div>
@@ -38,6 +50,7 @@ function App() {
 				value={professionalEmail()}
 				onChange={e => setProfessionalEmail(e.currentTarget.value)}
 			/>
+			<pre>{JSON.stringify(currentUsers(), null, 2)}</pre>
 		</div>
 	);
 }
