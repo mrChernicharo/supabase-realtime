12b1ad798ac0cd83bf487110ecc687d14e1a1a0e
Felipe Chernicharo <string7dev@gmail.com>
2022-10-09 01:47:19 -0400

    preparing badge for customers with offers

--- src/Customers.jsx
+++ src/Customers.jsx
@@ -3,11 +3,33 @@ import { createSignal } from "solid-js";
 import { store, addCustomer, removeCustomer } from "./store";
 import CustomerDetails from "./CustomerDetails";
 import { createMemo } from "solid-js";
+import { createEffect } from "solid-js";
+
+const s = {
+  badge: {
+    width: "12px",
+    height: "12px",
+    position: "absolute",
+    background: "red",
+    "border-radius": "50%",
+  },
+};
 
 export default function Customers() {
   const [currCustomerId, setCurrCustomerId] = createSignal(null);
+  const [hasOffers, setHasOffers] = createSignal(false);
 
   const currUser = createMemo(() => store.customers.find((c) => c.id === currCustomerId()));
+  const currCustomerOffers = createMemo(() =>
+    store.customers.find((c) => c.id === currCustomerId())
+  );
+
+  createEffect(() => {
+    if (currCustomerOffers()) {
+      console.log({ currCustomerOffers: currCustomerOffers().appointmentOffers });
+      setHasOffers(currCustomerOffers().appointmentOffers.length);
+    }
+  });
 
   return (
     <div>
@@ -30,6 +52,9 @@ export default function Customers() {
       <For each={store.customers}>
         {(person) => (
           <div className="d-flex">
+            <Show when={person.id === currCustomerId() && hasOffers()}>
+              <div style={s.badge}></div>
+            </Show>
             <p onClick={(e) => setCurrCustomerId(person.id)}>
               {person.name} : {person.email}
             </p>
